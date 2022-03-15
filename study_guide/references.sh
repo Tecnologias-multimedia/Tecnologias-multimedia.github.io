4c7fad468d269996723293b09fa6151bc5b30cb1
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2022-03-15 13:37:10 +0100

    .

---
+++ study_guide/references.sh
@@ -0,0 +1,13 @@
+export BIBINPUTS=":.:/home/vruiz/bibs/:"
+make clean
+make index.pdf
+rm -f references
+grep citation framework/index.aux >> references
+grep citation minimal/index.aux >> references
+grep citation latency/index.aux >> references
+grep citation BR_control/index.aux >> references
+grep citation transform_coding/index.aux >> references
+cat references >> index.aux
+bibtex index
+make index.html
+
