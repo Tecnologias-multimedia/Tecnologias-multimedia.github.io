1796bfd1922123341f25aed51a79ad76c61a0b02
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2022-09-14 13:11:00 +0200

    .

--- runme.sh
+++ runme.sh
@@ -2,14 +2,15 @@
 #make clean
 #make index.html
 make -C study_guide
-rm -f citations
-grep citation ~/the_sound/index.aux >> citations
-grep citation ~/human_auditory_system/index.aux >> citations
-grep citation ~/human_sound_perception/index.aux >> citations
-grep citation ~/the_light/index.aux >> citations
-grep citation ~/human_visual_system/index.aux >> citations
-grep citation ~/human_light_perception/index.aux >> citations
-cat citations >> index.aux
-bibtex index
+#rm -f citations
+#grep citation ~/the_sound/index.aux >> citations
+#grep citation ~/human_auditory_system/index.aux >> citations
+#grep citation ~/human_sound_perception/index.aux >> citations
+#grep citation ~/the_light/index.aux >> citations
+#grep citation ~/human_visual_system/index.aux >> citations
+#grep citation ~/human_light_perception/index.aux >> citations
+#cat citations >> index.aux
+#bibtex index
+
 make index.html
 
