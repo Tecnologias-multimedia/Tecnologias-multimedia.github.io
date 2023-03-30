8afeb057258ca1e2a0710ac6c89d8057f595ff1f
Vicente González-Ruiz <vruiz@ual.es>
2023-03-30 08:36:53 +0200

    .

---
+++ references/runme_and_forget_Makefile.sh
@@ -0,0 +1,17 @@
+export BIBINPUTS=":.:/home/vruiz/bibs/:"
+make clean
+make index.pdf
+rm -f citations
+grep citation ~/repos/Tecnologias-multimedia.github.io/contents/framework/index.aux >> citations
+grep citation ~/repos/Tecnologias-multimedia.github.io/contents/minimal/index.aux >> citations
+grep citation ~/repos/Tecnologias-multimedia.github.io/contents/NAT_traversal/index.aux >> citations
+grep citation ~/repos/Tecnologias-multimedia.github.io/contents/latency/index.aux >> citations
+grep citation ~/repos/Tecnologias-multimedia.github.io/contents/echo_cancellation/index.aux >> citations
+grep citation ~/repos/Tecnologias-multimedia.github.io/contents/threshold_of_hearing/index.aux >> citations
+grep citation ~/repos/Tecnologias-multimedia.github.io/contents/simultaneous_masking/index.aux >> citations
+grep citation ~/repos/Tecnologias-multimedia.github.io/contents/BR_control/index.aux >> citations
+grep citation ~/repos/Tecnologias-multimedia.github.io/contents/transform_coding/index.aux >> citations
+cat citations >> index.aux
+bibtex index
+make index.html
+
