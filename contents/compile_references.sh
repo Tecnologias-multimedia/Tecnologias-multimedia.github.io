267a77fd8145faed5962be4c9562f093b36f9ee4
vruiz <vicente.gonzalez.ruiz@gmail.com>
2024-02-02 23:35:26 +0100

    .

--- contents/compile_references.sh
+++ contents/compile_references.sh
@@ -4,10 +4,14 @@ make index.pdf
 rm -f references
 grep citation framework/index.aux >> references
 grep citation minimal/index.aux >> references
-grep citation latency/index.aux >> references
+grep citation NAT_traversal/index.aux >> references
+grep citation jitter/index.aux >> references
+grep citation echo_cancellation/index.aux >> references
 grep citation BR_control/index.aux >> references
 grep citation transform_coding/index.aux >> references
+grep citation high_resolution_ToH/index.aux >> references
+grep citation custom_ToH/index.aux >> references
+grep citation simultaneous_masking/index.aux >> references
 cat references >> index.aux
 bibtex index
 make index.html
-
