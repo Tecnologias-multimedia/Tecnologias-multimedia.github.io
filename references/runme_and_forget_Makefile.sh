7a1f83f56c0dda0bd4d20364bb9075c83dcfa10f
vruiz <vicente.gonzalez.ruiz@gmail.com>
2024-02-04 00:18:02 +0100

    .

--- references/runme_and_forget_Makefile.sh
+++ references/runme_and_forget_Makefile.sh
@@ -5,12 +5,13 @@ rm -f citations
 grep citation ~/repos/Tecnologias-multimedia.github.io/contents/framework/index.aux >> citations
 grep citation ~/repos/Tecnologias-multimedia.github.io/contents/minimal/index.aux >> citations
 grep citation ~/repos/Tecnologias-multimedia.github.io/contents/NAT_traversal/index.aux >> citations
-grep citation ~/repos/Tecnologias-multimedia.github.io/contents/latency/index.aux >> citations
+grep citation ~/repos/Tecnologias-multimedia.github.io/contents/jitter/index.aux >> citations
 grep citation ~/repos/Tecnologias-multimedia.github.io/contents/echo_cancellation/index.aux >> citations
 grep citation ~/repos/Tecnologias-multimedia.github.io/contents/threshold_of_hearing/index.aux >> citations
 grep citation ~/repos/Tecnologias-multimedia.github.io/contents/simultaneous_masking/index.aux >> citations
 grep citation ~/repos/Tecnologias-multimedia.github.io/contents/BR_control/index.aux >> citations
 grep citation ~/repos/Tecnologias-multimedia.github.io/contents/transform_coding/index.aux >> citations
+grep citation ~/repos/Tecnologias-multimedia.github.io/contents/high_resolution_ToH/index.aux >> citations
 cat citations >> index.aux
 bibtex index
 make index.html
